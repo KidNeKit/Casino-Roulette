eb623c9a2c9439e3329f443fbc00ab5f1246228f
Nikita Menshikov <menshikov.mikita@gmail.com>
2021-09-19 14:10:29 +0300

    First commit

---
+++ src/main/java/com/mercury/roulette/CasinoRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.mercury.roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoRouletteApplication.class, args);
+	}
+
+}
